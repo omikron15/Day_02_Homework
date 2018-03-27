b64755b83dd012e38cccab25890ba0a42d5277d8
omikron15 <omikron15@hotmail.co.uk>
2018-03-27 14:51:18 +0100

    Second update

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,36 @@
 def add(num_1, num_2)
   return num_1 + num_2
 end
+
+def number_to_full_month_name(num_1)
+
+    case num_1
+    when 1
+      return "January"
+    when 2
+      return "February"
+    when 3
+      return "March"
+    when 4
+      return "April"
+    when 5
+      return "May"
+    when 6
+      return "June"
+    when 7
+      return "July"
+    when 8
+      return "August"
+    when 9
+      return "September"
+    when 10
+      return "October"
+    when 11
+      return "November"
+    when 12
+      return "December"
+    end
+  
+
+
+end
