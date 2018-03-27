086a97f4eee6213a7c662d4e6008f8e31f155d0b
omikron15 <omikron15@hotmail.co.uk>
2018-03-27 15:03:37 +0100

    sixth commit

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -18,6 +18,10 @@ def add_string_as_number(str_1, str_2)
 return str_1.to_i + str_2.to_i
 end
 
+def join_string( string_1, string_2 )
+return string_1 + string_2
+end
+
 def number_to_full_month_name(num_1)
 
     case num_1
